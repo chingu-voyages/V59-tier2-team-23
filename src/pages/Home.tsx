1f520456945d9a6918f6abe10764f7313c789a81
Bryan Hoyem <bjhoyem@gmail.com>
2026-01-21 22:56:33 -0600

    Initial draft of homepage
    Created the initial draft of the homepage with a dummy logo and text. The button is not yet functional.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,8 +6,19 @@ type Props = {
 
 export default function Home({ className = "", ...props }: Props): JSX.Element {
   return (
-    <div className={`${className} h-9/12`} {...props}>
-      <h1 className="text-[4rem] ">IAM HOME</h1>
+    <div className={`${className} h-8/12 flex flex-col items-center w-4/5 mx-auto text-center`} {...props}>
+      <img src="public\images\temp-logo.png" alt="App Logo" className="h-1/4"/>
+      <h1 className="text-[2rem] sm:text-[3rem] md:text-[4rem]">WELCOME TO QUIZEST</h1>
+      <p className="text-[1.2rem]">Explanation / description / promotion of the app here. Feel free to come up with ideas for this section.</p>
+      {/* <select className="border p-2 rounded mt-[10px]">
+        <option value="">Choose Your Role</option>
+        <option value="scrummaster">Scrum Master</option>
+        <option value="productowner">Product Owner</option>
+        <option value="uiuxdesigner">UI/UX Designer</option>
+        <option value="webdeveloper">Web Developer</option>
+        <option value="pythondeveloper">Python Developer</option>
+      </select> */}
+      <button className="border p-2 rounded mt-[10px]">Choose Your Role</button>
     </div>
   );
 }
