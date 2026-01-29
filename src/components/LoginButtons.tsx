111bde965d950b47b068f23c801cf83f5f922dae
ivannissimrch <ivannissimrch@gmail.com>
2026-01-29 08:57:23 -0500

    Adds GitHub login button

--- src/components/LoginButtons.tsx
+++ src/components/LoginButtons.tsx
@@ -1,28 +1,55 @@
 import { Link } from "react-router-dom";
-import { useAuth } from "../context/AuthContext";
+import { useAuth, type LoginProvider } from "../context/AuthContext";
 import { FcGoogle } from "react-icons/fc";
 import { useState } from "react";
-import { FaSpinner } from "react-icons/fa";
+import { FaSpinner, FaGithub } from "react-icons/fa";
 
 export default function LoginButtons() {
-  const { user, signInWithGoogle } = useAuth();
-  const [isLogging, setIsLogging] = useState(false);
+  const { user, signIn } = useAuth();
+  const [isGoogleLogging, setIsGoogleLogging] = useState(false);
+  const [isGitHubLogging, setIsGitHubLogging] = useState(false);
 
-  async function handleLogging() {
-    setIsLogging(true);
-    await signInWithGoogle();
+  async function handleLogging(authProvider: LoginProvider) {
+    if (authProvider === "google") {
+      setIsGoogleLogging(true);
+      await signIn(authProvider);
+    }
+    if (authProvider === "github") {
+      setIsGitHubLogging(true);
+      await signIn(authProvider);
+    }
   }
 
   if (!user) {
     return (
-      <div className="font-bold text-[1.5rem] py-5">
+      <div className="font-bold text-[1.5rem] py-5 gap-2 flex flex-col md:flex-row">
         <button
           className="border p-2 rounded mt-2.5 flex items-center gap-2 cursor-pointer hover:bg-gray-200 transition-all"
-          onClick={handleLogging}
-          disabled={isLogging}
+          onClick={() => handleLogging("google")}
+          disabled={isGoogleLogging || isGitHubLogging}
         >
-          {isLogging ? <FaSpinner className="animate-spin" /> : <FcGoogle />}
-          {isLogging ? "Logging in..." : "Login with Google"}
+          {isGoogleLogging ? (
+            <div className=" w-60 h-9 flex justify-center items-center">
+              <FaSpinner className="animate-spin " />
+            </div>
+          ) : (
+            <FcGoogle />
+          )}
+          {isGoogleLogging ? "" : "Login with Google"}
+        </button>
+        <button
+          className="border p-2 rounded mt-2.5 flex items-center gap-2 cursor-pointer hover:bg-gray-200 transition-all"
+          onClick={() => handleLogging("github")}
+          disabled={isGitHubLogging || isGoogleLogging}
+        >
+          {isGitHubLogging ? (
+            <div className=" w-60 h-9 flex justify-center items-center">
+              <FaSpinner className="animate-spin " />
+            </div>
+          ) : (
+            <FaGithub />
+          )}
+          {isGitHubLogging ? "" : "Login with GitHub"}
         </button>
       </div>
     );
