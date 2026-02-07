f3caf2703eb938cdfe985cb6f4a151514b485fd6
Emily <devemployed@gmail.com>
2026-02-07 15:26:00 -0500

    add first working stat board

--- src/components/LB_Stat_Card.tsx
+++ src/components/LB_Stat_Card.tsx
@@ -1,3 +1,5 @@
+import type { sortedUserType } from "../pages/Leaderboard"
+
 type IndividualUserStats = {
   name: string
   place: number
@@ -5,41 +7,26 @@ type IndividualUserStats = {
   metricType?: "Grade" | "Quantity Studied" // probably will delete this line
 }
 
-type TopTenStatCard = {
-  users: IndividualUserStats[]
-}
+// type TopTenStatCard = {
+//   users: IndividualUserStats[]
+// }
 
-export default function LeaderboardStatCard() {
+export default function LeaderboardStatCard({
+  topTenArray,
+}: {
+  topTenArray: sortedUserType[] | []
+}) {
+  console.log(typeof topTenArray, topTenArray)
   return (
     <div className='flex flex-col items-center pt-10'>
-      {/* CREATE A TABLE HERE USING FLEX OR GRID (probably grid and make sure boundaries don't move, worst case a very long username is cut off or scrolls or something) */}
       <div className='flex flex-col bg-white/20  border-4  border-white rounded-xl'>
-        <div className='flex relative w-100 pt-1.5 pb-1.5 border-2  border-white '>
-          <div className='font-bold sticky left-0 pl-2'> #1</div>
-          <div className='text-center pl-2 pr-2'> John Smith </div>
-          <div className='absolute right-2'> Metric </div>
-        </div>
-        {/* temp placeholder below but they will be displayed dynamically*/}
-        <div className='flex relative w-100 pt-1.5 pb-1.5  border-2  border-white '>
-          <div className='font-bold sticky left-0 pl-2'> #2 </div>
-          <div className='text-center pl-2 pr-2'> Jane Doe </div>
-          <div className='absolute right-2'> Metric </div>
-        </div>
-        <div className='flex relative w-100 pt-1.5 pb-1.5  border-2  border-white '>
-          <div className='font-bold   left-0 pl-2'> #3 </div>
-          <div className='text-center pl-2 pr-2'>Ebenezer Dude</div>
-          <div className='absolute right-2'> Metric </div>
-        </div>
-
-        <IndividualUserStat name='John Doe' place={1} metricNumber={4535}></IndividualUserStat>
-        <IndividualUserStat name='John Doe' place={1} metricNumber={4535}></IndividualUserStat>
-        <IndividualUserStat name='John Doe' place={1} metricNumber={4535}></IndividualUserStat>
-        <IndividualUserStat name='John Doe' place={1} metricNumber={4535}></IndividualUserStat>
-        <IndividualUserStat name='John Doe' place={1} metricNumber={4535}></IndividualUserStat>
-        <IndividualUserStat name='John Doe' place={1} metricNumber={4535}></IndividualUserStat>
-        <IndividualUserStat name='John Doe' place={1} metricNumber={4535}></IndividualUserStat>
-        <IndividualUserStat name='John Doe' place={1} metricNumber={4535}></IndividualUserStat>
-        <IndividualUserStat name='John Doe' place={1} metricNumber={4535}></IndividualUserStat>
+        {topTenArray.map((u, i) => (
+          <IndividualUserStat
+            key={u.userId}
+            name={u.userName}
+            place={i + 1}
+            metricNumber={u.totalQuestions}></IndividualUserStat>
+        ))}
       </div>
     </div>
   )
--- src/pages/Leaderboard.tsx
+++ src/pages/Leaderboard.tsx
@@ -9,12 +9,37 @@ import {
 // import { useUserData } from "../utils/fetchStats"
 import { useState, useEffect } from "react"
 
+export type SessionType = {
+  completed_at: string
+  id: string
+  role_id: string
+  score: number
+  started_at: string
+  total_questions: number
+  user_id: string
+  user_name: string
+}
+
+export type sortedUserType = {
+  userId: string
+  userName: string
+  totalSessions: number
+  totalQuestions: number
+  totalScore: number
+}
+
 export default function Leaderboard() {
   const [userData, setUserData] = useState<User | null>(null)
   const [loadingUser, setLoadingUser] = useState(true)
 
+  const [allSessionData, setAllSessionData] = useState<SessionType[] | null>(null)
+  const [loadingAllSessions, setLoadingAllSessions] = useState(true)
+
   const firstName = userData?.user_metadata.name.split(" ")[0]
   const lastName = userData?.user_metadata.name.split(" ")[1]
+  const sortedSessions = sortAllSessions(allSessionData) || []
+  const topUsers = topUsersByNumberOfQuestions(sortedSessions)
+  // console.log(sortedSessions)
 
   useEffect(() => {
     async function fetchUser() {
@@ -23,8 +48,51 @@ export default function Leaderboard() {
       setLoadingUser(false)
     }
 
+    async function fetchAllSessions() {
+      const sessions = await getSessions()
+      setAllSessionData(sessions)
+      setLoadingAllSessions(false)
+    }
+
     fetchUser()
+    fetchAllSessions()
   }, [])
+
+  function sortAllSessions(sessions: SessionType[] | null) {
+    if (!sessions || sessions === null || sessions === undefined) return
+    let sortedSessions: sortedUserType[] = []
+    for (let i = 0; i <= sessions.length - 1; i++) {
+      let selectedSession =
+        sortedSessions.find((session) => session.userId == sessions[i].user_id) || undefined
+
+      if (selectedSession) {
+        selectedSession.totalSessions += 1
+        selectedSession.totalQuestions += sessions[i].total_questions
+        selectedSession.totalScore += sessions[i].score
+      } else {
+        sortedSessions.push({
+          userId: sessions[i].user_id,
+          userName: sessions[i].user_name,
+          totalSessions: 1,
+          totalQuestions: sessions[i].total_questions,
+          totalScore: sessions[i].score,
+        })
+      }
+    }
+    return sortedSessions
+  }
+
+  function topUsersByNumberOfQuestions(sortedSessions: sortedUserType[] | null) {
+    if (sortedSessions === null || sortedSessions == undefined || !sortedSessions) return
+    let topUsers: sortedUserType[] = []
+
+    for (let i = 0; i <= 9; i++) {
+      if (sortedSessions[i]) topUsers.push(sortedSessions[i])
+    }
+
+    topUsers = topUsers.sort((a, b) => b.totalQuestions - a.totalQuestions)
+    return topUsers
+  }
 
   /*PLANNING SESSION - 
 So I'm going to access the real data, figure out exactly what I'm using, 
@@ -35,33 +103,22 @@ In the LeaderBoardComponents, I will have a big card for each "Top 10 topic" and
 For topics, I'll begin with overall number of q.s answered and overall correctness, before diving into those metrics per ind. role studied for
 */
 
-  if (loadingUser) {
+  if (loadingUser || loadingAllSessions) {
     return <div> Loading... </div>
   }
   return (
     <div className='flex flex-col items-center bg-linear-to-br from-indigo-400 to-purple-500 pb-25 '>
-      {/* 
-      
-      So the whole things needs to be minimum, almost enough to cover the screen height wise
-
-      Needs to have a TTILE
-
-      And a list of rankings (Top 10) for 1-3 stats - make it a little bit colorful for now
-      
-      
-      */}
-
       <div className='text-center p-6 text-5xl '>
         <h1>Welcome to the Leaderboard, {firstName}!</h1>
       </div>
 
       <div>
         <div className='text-center p-4 text-2xl '>
-          <h1>Display data for- Top Ten Users for Having Answered the Most Questions</h1>
+          <h1>Users That Have Answered The Most Questions</h1>
         </div>
 
         <div>
-          <LeaderboardStatCard />
+          <LeaderboardStatCard topTenArray={topUsers || []} />
         </div>
       </div>
     </div>
